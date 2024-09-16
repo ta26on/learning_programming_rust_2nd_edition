53f3e57ac1fd44258402918905b517a9718a9cec
OTA Tatsuro <ta26on@gmail.com>
2024-09-16 14:32:02 +0900

    ch.02 fn gcd

---
+++ chapter02/hello/target/debug/deps/hello-4f19f1a9699bcf43.d
@@ -0,0 +1,5 @@
+/Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/target/debug/deps/hello-4f19f1a9699bcf43: src/main.rs
+
+/Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/target/debug/deps/hello-4f19f1a9699bcf43.d: src/main.rs
+
+src/main.rs:
---
+++ chapter02/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/target/debug/hello: /Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/src/main.rs
