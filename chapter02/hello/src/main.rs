25247abe843500dc45151c5064d881901daef32b
OTA Tatsuro <ta26on@gmail.com>
2024-09-16 14:57:50 +0900

    command line interface

--- chapter02/hello/src/main.rs
+++ chapter02/hello/src/main.rs
@@ -1,8 +1,29 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("gcd of {} and {} is {}", 1071, 1029, gcd(1071,1029));
+    println!("gcd of {} and {} is {}", 1071, 1029, gcd(1071, 1029));
+
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
-fn gcd(mut n: u64, mut m: u64) -> u64{
+fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
@@ -16,10 +37,7 @@ fn gcd(mut n: u64, mut m: u64) -> u64{
 }
 
 #[test]
-fn test_gcd(){
-    assert_eq!(gcd(14,15), 1);
-    assert_eq!(gcd( 2 * 3 * 5 * 11 * 17,
-                    3 * 7 * 11 * 13 * 19), 
-                    3 * 11);
-    
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
