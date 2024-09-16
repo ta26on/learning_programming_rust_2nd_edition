c523a5dd18df990d2eb7e1b92c15875b5bf7d598
OTA Tatsuro <ta26on@gmail.com>
2024-09-16 14:39:47 +0900

    add test_gcd

---
+++ chapter02/hello/target/debug/deps/hello-14a21692cf22f7f8.d
@@ -0,0 +1,5 @@
+/Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/target/debug/deps/hello-14a21692cf22f7f8: src/main.rs
+
+/Users/ta26on/projects/rust/learning_programming_rust_2nd_edition/chapter02/hello/target/debug/deps/hello-14a21692cf22f7f8.d: src/main.rs
+
+src/main.rs:
